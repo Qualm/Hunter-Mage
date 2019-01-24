87f731948a95bb05b7a1be82d0a95b20c548e468
GBC Pier <poboivin2@gmail.com>
2019-01-24 17:13:43 -0500

    no message

---
+++ source/HunterMage/Source/HunterMage/HunterMage.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HunterMage.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HunterMage, "HunterMage" );
